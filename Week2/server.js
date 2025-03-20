b9af5b0f009e70cc44a6c8e9e807a719d4e0dd2d
Zixuan-Han <hanzixuan2023@gmail.com>
2025-03-20 20:11:03 +1100

    Added Week2 folder

---
+++ Week2/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+let quotes = [
+    "The best way to predict the future is to invent it.",
+    "Life is 10% what happens to us and 90% how we react to it.",
+    "The only limit to our realization of tomorrow is our doubts of today.",
+    "Do not wait to strike till the iron is hot; but make it hot by striking."
+];
+
+app.get('/api/quote', (req, res) => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    res.json({ quote: quotes[randomIndex] });
+});
+
+app.listen(PORT, () => {
+    console.log("App listening to: " + PORT);
+});
