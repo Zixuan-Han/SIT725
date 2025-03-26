68f15aacdedbd555d54b639cd067695a667f2294
Zixuan-Han <hanzixuan2023@gmail.com>
2025-03-26 19:10:02 +1100

    Added Week3 folder

---
+++ Week3/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, () => {
+  console.log("App listening to: " + port);
+});
